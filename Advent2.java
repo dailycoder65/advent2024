ddb008424394ec60a1c67633406bf6e4e891473d
dailycoder65 <dailycoder65@gmail.com>
2024-12-05 10:38:33 -0800

    Updated

--- Advent2.java
+++ Advent2.java
@@ -21,13 +21,38 @@ public class Advent2  {
         }
 
 
-        int [] myIntArray = getIntArray(list.get(2));
+        int [] myIntArray = getIntArray(list.get(3));
         System.out.println(Arrays.toString(myIntArray));
-//         for (String report: list) {
+//         for (String report: list) :w
 //             
 //         }
 
+        int [] arr1 = {1,2,3,4,5,5};
+        int [] arr2 = {5,4,3,2,1,1};
+        int [] arr3 = {8,4,1,9,7,6};
+        System.out.println(isIncreasing(arr1));
+        System.out.println(isIncreasing(arr2));
+        System.out.println(isIncreasing(arr3));
     }
+
+    public static boolean isIncreasing(int [] arr) {
+        boolean increasing = true;
+        int spacing;
+        for (int i=0; i < arr.length; i++) {
+//            System.out.println(arr[i+1] + " " + arr[i]);
+ //           spacing = arr[i+i] - arr[i];
+  //          System.out.println("spacing: " + spacing);
+            if (arr[i+1] <= arr[i] && (arr[i+1] - arr[i]) >= 1 && (arr[i+1] - arr[i]) <=3 )
+                return false;
+        }
+        return increasing;
+    }
+
+    public static boolean isDecreasing(int [] arr) {
+
+        return false;
+    }
+
 
     public static int[] getIntArray(String report) {
         String [] strArray = report.split(" ");
